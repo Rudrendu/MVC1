cc1b7acd0b19fd0f0dc1637541810265d6ddaa5e
rudrendu.roy <rudrendu.roy1@outlook.com>
2017-12-02 23:41:39 +0530

    V1

---
+++ MVC1/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC1.Models;
+
+namespace MVC1.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { ID = 1, Name = "Shrek!" };
+            return View(movie);
+        }
+    }
+}
